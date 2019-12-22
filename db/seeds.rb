ee47039ac7a43319b34216064b6df1b030187212
Dantokoro <duongdang0508@gmail.com>
2019-12-22 15:50:52 +0000

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: 'Example User',
+User.create!(name: 'Mr.Example',
              email: 'example@railstutorial.org',
              password: 'foobar',
              password_confirmation: 'foobar',
